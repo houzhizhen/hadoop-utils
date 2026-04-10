a97a9e4a5b522007b25441ac8db5a58318824b29
houzhizhen <houzhizhen@baidu.com>
2026-04-10 16:27:30 +0800

    delete unused parallel file

--- bin/hdfs-lock-test/paralle-write-test.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-export TAG=$1-`date +"%Y%m%d-%H%M%S"`
-mkdir -p ${TAG}
-sh 1000-thread.sh > ${TAG}/${TAG}-1000-thread.log 2>&1
-sh 2000-thread.sh > ${TAG}/${TAG}-2000-thread.log 2>&1
-sh 4000-thread.sh > ${TAG}/${TAG}-4000-thread.log 2>&1
-sh 8000-thread.sh > ${TAG}/${TAG}-8000-thread.log 2>&1
-sh 12000-thread.sh > ${TAG}/${TAG}-12000-thread.log 2>&1
--- bin/hdfs-lock-test/paralles-hdd-all.sh
+++
@@ -1,5 +0,0 @@
-sh use_share_old.sh
-sh paralle-write-test.sh "parallel-hdd-before-opt"
-
-sh use_share_new.sh
-sh paralle-write-test.sh "parallel-hdd-after-opt" 
